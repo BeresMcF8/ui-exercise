73931e13a251600b1be0e1c7f807f14a50f9cd7f
Beres <Beres.McFarlane@hotmail.com>
2018-01-18 21:23:15 +0000

    End of exercise 3.

--- ui-exercise/test.js
+++ ui-exercise/test.js
@@ -1,1 +1,13 @@
-﻿
+﻿$(function ($) {
+    var hideSidebar = false;
+    $('.body-content .navbar-collapse ul li.sidebar').click(function (e) {
+        e.preventDefault();
+        if (!hideSidebar) {
+            $('.sidebar-container').hide();
+            hideSidebar = true;
+        } else {
+            $('.sidebar-container').show();
+            hideSidebar = false;
+        }
+    });
+})(jQuery)
